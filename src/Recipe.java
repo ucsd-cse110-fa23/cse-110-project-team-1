225db6734e784e401ce996fedf2a2862cfe2ed68
mBookUCSD <93008597+mBookUCSD@users.noreply.github.com>
2023-10-31 14:05:18 -0700

    started recipe and recipelist classes

---
+++ src/Recipe.java
@@ -0,0 +1,25 @@
+public class Recipe {
+    
+
+    int index;
+    String title;
+    String recipeText;
+
+    Recipe(int index, String titie, String recipeText){
+
+    }
+
+    int getIndex(){
+        
+    }
+
+    String getTitle(){}
+
+    String getRecipeText(){}
+
+    void setTitle(){}
+
+    void setRecipeText(){}
+
+}
+
---
+++ src/RecipeList.java
@@ -0,0 +1,7 @@
+public class RecipeList {
+    
+    int count;
+    
+
+}
+
