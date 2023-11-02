7a5a94b6ed601b37979d6e5024d1e6cf3170bd2e
nskrable <78564330+nskrable@users.noreply.github.com>
2023-11-01 18:24:52 -0700

    Add initial project files and main method
    javafx-sdk-21 folder goes in /lib/

---
+++ src/RecipeApp/Main.java
@@ -0,0 +1,19 @@
+package RecipeApp;
+
+import java.io.*;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		System.out.println("HEY GUYZ.");
+	}
+    
+}
