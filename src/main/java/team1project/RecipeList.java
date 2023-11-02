2bf634c6b34932c239fe2b80efa4740b1b20f92c
Anshal Jain <pk.ness.ness@gmail.com>
2023-11-01 21:18:34 -0700

    changed newRecipe to addRecipe, added a test

--- src/main/java/team1project/RecipeList.java
+++ src/main/java/team1project/RecipeList.java
@@ -88,7 +88,7 @@ public class RecipeList {
     public Recipe getRecipe(int recipeID){
         return recipeList.get(recipeID);
     }
-    public void newRecipe(String recipeTitle, String recipeText){
+    public void addRecipe(String recipeTitle, String recipeText){
         int recipeID = ++highestIndex; //increment highestIndex with every new recipe creataed
         Recipe r = new Recipe(recipeID,recipeTitle, recipeText);
         recipeList.put(recipeID,r);
--- src/test/java/team1project/RecipeTest.java
+++ src/test/java/team1project/RecipeTest.java
@@ -2,6 +2,7 @@ package test.java.team1project;
 import org.junit.jupiter.api.Test;
 
 import main.java.team1project.Recipe;
+import main.java.team1project.RecipeList;
 
 import org.junit.jupiter.api.BeforeEach;
 
@@ -33,6 +34,23 @@ public class RecipeTest { //temporary tests until JUnit is in place\
         assertEquals(newRecipeID, newRecipe.getRecipeID());
         assertEquals(newRecipeText, newRecipe.getRecipeText());
         assertEquals(newRecipeTitle, newRecipe.getRecipeTitle());
+
+    }
+    @Test
+    void testRecipeListConstruction(){
+
+        String newRecipeTitle = "Souffle";
+        String newRecipeText = "Souffle Recipe\n Ingredients:\n2 Jars of eggs\n\nInstructions:\nStep 1: Put egg in ramen\n";
+
+        RecipeList newRecipeList = new RecipeList();
+
+        newRecipeList.addRecipe(newRecipeTitle, newRecipeText);
+
+
+
+        assertEquals(1, newRecipeList.getRecipe(1).getRecipeID());
+        assertEquals(newRecipeText, newRecipeList.getRecipe(1).getRecipeText());
+        assertEquals(newRecipeTitle, newRecipeList.getRecipe(1).getRecipeTitle());
 
     }
     /*public static void main(String[] args){
