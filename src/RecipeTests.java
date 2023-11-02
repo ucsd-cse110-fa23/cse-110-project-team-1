10f015a9f009b1123f07351af98e0566b1b05804
mBookUCSD <93008597+mBookUCSD@users.noreply.github.com>
2023-11-01 20:10:40 -0700

    added junit and gradle testing

--- src/RecipeTests.java
+++ src/RecipeTests.java
@@ -1,5 +1,37 @@
-public class RecipeTests { //temporary tests until JUnit is in place
-    public static void main(String[] args){
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.beans.Transient;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RecipeTests { //temporary tests until JUnit is in place\
+    private Recipe recipe;
+
+    @BeforeEach
+    void setUp(){
+
+    }
+    @Test
+    void testRecipeConstruction(){
+        int newRecipeID = 123;
+        String newRecipeTitle = "Beans";
+        String newRecipeText = "Beans Recipe\n Ingredients:\n1 Can of Beans\n\nInstructions:\nStep 1: Put beans on plate\n";
+
+        Recipe newRecipe = new Recipe(newRecipeID,newRecipeTitle,newRecipeText);
+
+
+
+        assertEquals(newRecipeID, newRecipe.getRecipeID());
+        assertEquals(newRecipeText, newRecipe.getRecipeText());
+        assertEquals(newRecipeTitle, newRecipe.getRecipeTitle());
+
+    }
+    /*public static void main(String[] args){
         Recipe L = new Recipe(0, "ball", "two eggs");
         Recipe G = new Recipe(1, "souffle", "three eggs");
 
@@ -17,5 +49,5 @@ public class RecipeTests { //temporary tests until JUnit is in place
         list2.loadFromDisk();
         System.out.println(list2);
 
-    }
+    }*/
 }
