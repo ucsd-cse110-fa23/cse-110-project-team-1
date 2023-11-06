5e73128b9c62574cbb8ab245f53cda0ee3b51765
mBookUCSD <93008597+mBookUCSD@users.noreply.github.com>
2023-11-05 21:48:30 -0800

    made recipes pull from server and display text

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -1,6 +1,12 @@
 package View;
 
+import Controller.*;
+import Model.RecipeServer;
+
 import java.io.*;
+import java.util.Iterator;
+
+import org.json.JSONObject;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -21,27 +27,51 @@ public class Main extends Application {
 
 	public final String appName = new String("Recipe App");
 	public final int dimX = 1000, dimY = 600;
+    public static RecipeServer server; 
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+	public ListView<HBox> pullRecipes(){
+		RequestHandler req = new RequestHandler();
+		String allRecipes = req.performGET("http://localhost:8100/?all");
+		JSONObject allRec = new JSONObject(allRecipes);
+		Iterator<String> keys = allRec.keys();
+		ListView<HBox> listView = new ListView<>();
+            while(keys.hasNext()) {
+				String key = keys.next();
+				JSONObject r = allRec.getJSONObject(key);
+                //System.out.println(allRec.getJSONObject(key));
+                //System.out.println(r.getString("recipeTitle"));
+				
+				RecipeNode newRecipe = new RecipeNode(r.getInt("recipeID"),r.getString("recipeTitle"),r.getString("recipeText"));
+				newRecipe.getChildren().add(new Label(newRecipe.getRecipeTitle()));
+                System.out.println(newRecipe.toString());
+				listView.getItems().add(newRecipe);
 
+			}
+		return listView;
+		//System.out.println(allRec.toString());
+	}
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
 		BorderPane root = new BorderPane();
 		root.setPadding(new Insets(10));
-
 		//Left ListView with placeholders and a + button
-		ListView<HBox> listView = new ListView<>();
-		int recipeNum = 5;
-		for (int i = 0; i < recipeNum; i++) {
+		server = new RecipeServer();
+		server.startServer();
+		pullRecipes();
+		
+		ListView<HBox> listView = pullRecipes();
+		//int recipeNum = 5;
+		//for (int i = 0; i < recipeNum; i++) {
 			//Placeholder image
-			ImageView imageView = new ImageView(new Image("file:src/main/java/View/images/" + (i + 1) + ".jpg"));
-			imageView.setFitHeight(50);
-			imageView.setFitWidth(50);
-			HBox hBox = new HBox(10, imageView, new Label("Recipe " + (i + 1)));
-			listView.getItems().add(hBox);
-		}
+			//ImageView imageView = new ImageView(new Image("file:src/main/java/View/images/" + (i + 1) + ".jpg"));
+			//imageView.setFitHeight(50);
+			//imageView.setFitWidth(50);
+			//HBox hBox = new HBox(10, imageView, new Label("Recipe " + (i + 1)));
+			//listView.getItems().add(hBox);
+		//}
 		VBox leftVBox = new VBox(10, listView, new Button("+"));
 		VBox.setVgrow(listView, Priority.ALWAYS);
 		root.setLeft(leftVBox);
@@ -63,7 +93,9 @@ public class Main extends Application {
 			if (newValue != null) {
 				int selectedIndex = listView.getSelectionModel().getSelectedIndex();
 				detailImage.setImage(new Image("file:./src/RecipeApp/images/" + (selectedIndex + 1) + ".jpg"));
-				recipeDescription.setText("Recipe description for recipe " + (selectedIndex + 1) + ": ...");
+
+				String recipeText = ((RecipeNode)listView.getSelectionModel().getSelectedItem()).getRecipeText();
+				recipeDescription.setText(recipeText);
 			}
 		});
 
