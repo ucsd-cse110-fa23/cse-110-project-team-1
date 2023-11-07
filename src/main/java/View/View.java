a535b52801abc9629bfb4e147221976d4015b5ac
mBookUCSD <93008597+mBookUCSD@users.noreply.github.com>
2023-11-06 20:35:18 -0800

    fixed update

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -36,28 +36,10 @@ public class View {
 		//Left ListView with placeholders and a + button
 
 		this.recipeQuery.setPromptText("Enter Recipe Query...");
-		ListView<HBox> listView = ViewModel.pullRecipes();
-
 		this.generateButton.setOnAction(e -> this.onGenerateRequest());
 
 		// left side
 		this.updateRecipes();
-
-		//Right side
-		this.rightVBox = new VBox(10);
-		Label recipeDescription = new Label("Recipe Description: ...");
-		this.rightVBox.getChildren().addAll(recipeDescription, new Button("Rectangle Button?"));
-		this.rightVBox.setAlignment(javafx.geometry.Pos.TOP_LEFT);
-		this.root.setRight(this.rightVBox);
-
-
-		//Update detail view
-		listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-			if (newValue != null) {
-				String recipeText = ((RecipeNode)listView.getSelectionModel().getSelectedItem()).getRecipeText();
-				recipeDescription.setText(recipeText);
-			}
-		});
 
 	}
 
@@ -68,10 +50,24 @@ public class View {
 	public void updateRecipes() {
 		System.out.println("printing the muhfuckin list viwew");
 
+		Label recipeDescription = new Label("Recipe Description: ...");
 		ListView<HBox> daStuff = ViewModel.pullRecipes();
 		this.leftVBox = new VBox(10, daStuff, new Button("+"), this.recipeQuery, this.generateButton);
 		VBox.setVgrow(daStuff, Priority.ALWAYS);
 		this.root.setLeft(this.leftVBox);
+		//Update detail view
+		daStuff.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue != null) {
+				String recipeText = ((RecipeNode)daStuff.getSelectionModel().getSelectedItem()).getRecipeText();
+				recipeDescription.setText(recipeText);
+			}
+		});
+
+		//Right side
+		this.rightVBox = new VBox(10);
+		this.rightVBox.getChildren().addAll(recipeDescription, new Button("Rectangle Button?"));
+		this.rightVBox.setAlignment(javafx.geometry.Pos.TOP_LEFT);
+		this.root.setRight(this.rightVBox);
 	}
 
 	private void onGenerateRequest() {
