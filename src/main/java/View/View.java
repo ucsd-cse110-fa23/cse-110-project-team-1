df2492a0b18f866f77c35d7388aa447b00b57bda
nskrable <78564330+nskrable@users.noreply.github.com>
2023-11-06 19:26:20 -0800

    generate recipe button
    button has no functionality yet

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -13,6 +13,7 @@ import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -20,64 +21,56 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.HBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class View {
 
-    public BorderPane root;
+	public BorderPane root;
 	public ListView<HBox> listView;
-    public View(){
-    root = new BorderPane();
-	root.setPadding(new Insets(10));
+	private TextField recipeQuery = new TextField();
+	private Button generateButton = new Button("Generate Recipe");
+
+	public View() {
+		this.root = new BorderPane();
+		this.root.setPadding(new Insets(10));
 		//Left ListView with placeholders and a + button
 
-		RequestHandler req = new RequestHandler();
-		req.performPOST("D,ingredients=butter\npotato");
-		
-		ListView<HBox> listView = ViewModel.pullRecipes();
-		//int recipeNum = 5;
-		//for (int i = 0; i < recipeNum; i++) {
-			//Placeholder image
-			//ImageView imageView = new ImageView(new Image("file:src/main/java/View/images/" + (i + 1) + ".jpg"));
-			//imageView.setFitHeight(50);
-			//imageView.setFitWidth(50);
-			//HBox hBox = new HBox(10, imageView, new Label("Recipe " + (i + 1)));
-			//listView.getItems().add(hBox);
-		//}
-		VBox leftVBox = new VBox(10, listView, new Button("+"));
-		VBox.setVgrow(listView, Priority.ALWAYS);
-		root.setLeft(leftVBox);
+		this.recipeQuery.setPromptText("Enter Recipe Query...");
+		this.listView = ViewModel.pullRecipes();
+
+		this.generateButton.setOnAction(e -> this.onGenerateRequest());
+
+		// left side
+		VBox leftVBox = new VBox(10, this.listView, new Button("+"), this.recipeQuery, this.generateButton);
+		VBox.setVgrow(this.listView, Priority.ALWAYS);
+		this.root.setLeft(leftVBox);
 
 		//Right side
 		VBox rightVBox = new VBox(10);
-		ImageView detailImage = new ImageView(new Image("file:./src/RecipeApp/images/1.jpg"));
-		detailImage.setFitHeight(400);
-		detailImage.setFitWidth(300);
 		Label recipeDescription = new Label("Recipe Description: ...");
-		rightVBox.getChildren().addAll(detailImage, recipeDescription, new Button("Rectangle Button?"));
+		rightVBox.getChildren().addAll(recipeDescription, new Button("Rectangle Button?"));
 		rightVBox.setAlignment(javafx.geometry.Pos.TOP_LEFT);
-		root.setRight(rightVBox);
+		this.root.setRight(rightVBox);
 
 
 		//Update detail view
-
-		listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+		this.listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
 			if (newValue != null) {
-				int selectedIndex = listView.getSelectionModel().getSelectedIndex();
-				detailImage.setImage(new Image("file:./src/RecipeApp/images/" + (selectedIndex + 1) + ".jpg"));
-
-				String recipeText = ((RecipeNode)listView.getSelectionModel().getSelectedItem()).getRecipeText();
+				String recipeText = ((RecipeNode)this.listView.getSelectionModel().getSelectedItem()).getRecipeText();
 				recipeDescription.setText(recipeText);
 			}
 		});
 
-    }
-    public BorderPane getRoot(){
-        return root;
-    }
-	public void updateRecipes(){
-		listView = ViewModel.pullRecipes();
 	}
 
+	public BorderPane getRoot() {
+		return this.root;
+	}
+	
+	public void updateRecipes() {
+		this.listView = ViewModel.pullRecipes();
+	}
+
+	private void onGenerateRequest() {
+		System.out.println("onGenerateRequest");
+	}
 }
