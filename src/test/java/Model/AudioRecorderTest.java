80ba9b0cb5be7f505fef059891c301e0035afcab
Kevin Kim <kevink856@gmail.com>
2023-11-08 15:24:58 -0800

    Add Long Test and Add Timeout to Both
    TODO: Check recording.wav file exists in the home dir

--- src/test/java/Model/AudioRecorderTest.java
+++ src/test/java/Model/AudioRecorderTest.java
@@ -4,12 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioSystem;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class AudioRecorderTest {
     private AudioRecorder audioRecorder;
@@ -20,19 +22,20 @@ public class AudioRecorderTest {
     }
 
     @Test
-    void testBasicRecording() {
+    @Timeout(value = 8 * 1000, unit = TimeUnit.MILLISECONDS)
+    void testShortRecording() {
         CountDownLatch latch = new CountDownLatch(1);
 
-        //Do your async job
+        //Async start and stop 1.5 sec recording
         Thread t = new Thread(
             new Runnable() {
             @Override
             public void run() {
                 audioRecorder.startRecording();
                 try {
-                    Thread.sleep(5000);
+                    Thread.sleep(1500);
                 } catch (Exception e1) {
-                    System.out.println(1111);
+                    System.err.println(e1);
                 }
                 audioRecorder.stopRecording();
                 latch.countDown();
@@ -40,7 +43,37 @@ public class AudioRecorderTest {
             });
         t.start();
 
-        //Wait for api response async
+        //Wait for async tasks (recording) to finish
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @Timeout(value = 15 * 1000, unit = TimeUnit.MILLISECONDS)
+    void testLongRecording() {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        //Async start and stop 10 sec recording
+        Thread t = new Thread(
+            new Runnable() {
+            @Override
+            public void run() {
+                audioRecorder.startRecording();
+                try {
+                    Thread.sleep(10000);
+                } catch (Exception e1) {
+                    System.err.println(e1);
+                }
+                audioRecorder.stopRecording();
+                latch.countDown();
+            }
+            });
+        t.start();
+
+        //Wait for async tasks (recording) to finish
         try {
             latch.await();
         } catch (InterruptedException e) {
