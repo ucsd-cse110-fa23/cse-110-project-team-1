8b55d31be82a5bb7cad7c0030b4b549c4395ac70
Kevin Kim <kevink856@gmail.com>
2023-11-09 16:41:23 -0800

    Reinitialize Audio Files

---
+++ src/main/java/Model/AudioRecorder.java
@@ -0,0 +1,81 @@
+package Model;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+public class AudioRecorder {
+    private AudioFormat audioFormat;
+    private TargetDataLine targetDataLine;
+
+    private AudioFormat getAudioFormat() {
+        // the number of samples of audio per second.
+        float sampleRate = 44100;
+        // the number of bits in each sample of a sound that has been digitized.
+        int sampleSizeInBits = 16;
+        // the number of audio channels in this format (1 for mono, 2 for stereo).
+        int channels = 1;
+        // whether the data is signed or unsigned.
+        boolean signed = true;
+        // whether the audio data is stored in big-endian or little-endian order.
+        boolean bigEndian = false;
+
+        return new AudioFormat(
+                sampleRate,
+                sampleSizeInBits,
+                channels,
+                signed,
+                bigEndian);
+    }
+
+    public AudioRecorder() {
+        this.audioFormat = this.getAudioFormat();
+    }
+
+    public void startRecording() {
+        try {
+            // the format of the TargetDataLine
+            DataLine.Info dataLineInfo = new DataLine.Info(
+                    TargetDataLine.class,
+                    audioFormat);
+            // the TargetDataLine used to capture audio data from the microphone
+            targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
+            targetDataLine.open(audioFormat);
+            targetDataLine.start();
+
+            // the AudioInputStream that will be used to write the audio data to a file
+            AudioInputStream audioInputStream = new AudioInputStream(
+                    targetDataLine);
+
+            // the file that will contain the audio data
+            File audioFile = new File("recording.wav");
+
+            Thread t = new Thread(
+                new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        AudioSystem.write(
+                            audioInputStream,
+                            AudioFileFormat.Type.WAVE,
+                            audioFile);
+                    } catch (Exception e1) {
+                    }
+                }
+                });
+            t.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void stopRecording() {
+        targetDataLine.stop();
+        targetDataLine.close();
+    }
+}
---
+++ src/test/java/Model/AudioRecorderTest.java
@@ -0,0 +1,83 @@
+package Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class AudioRecorderTest {
+    private AudioRecorder audioRecorder;
+
+    @BeforeEach
+    void setUp() {
+        audioRecorder = new AudioRecorder();
+    }
+
+    @Test
+    @Timeout(value = 8 * 1000, unit = TimeUnit.MILLISECONDS)
+    void testShortRecording() {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        //Async start and stop 1.5 sec recording
+        Thread t = new Thread(
+            new Runnable() {
+            @Override
+            public void run() {
+                audioRecorder.startRecording();
+                try {
+                    Thread.sleep(1500);
+                } catch (Exception e1) {
+                    System.err.println(e1);
+                }
+                audioRecorder.stopRecording();
+                latch.countDown();
+            }
+            });
+        t.start();
+
+        //Wait for async tasks (recording) to finish
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @Timeout(value = 15 * 1000, unit = TimeUnit.MILLISECONDS)
+    void testLongRecording() {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        //Async start and stop 10 sec recording
+        Thread t = new Thread(
+            new Runnable() {
+            @Override
+            public void run() {
+                audioRecorder.startRecording();
+                try {
+                    Thread.sleep(10000);
+                } catch (Exception e1) {
+                    System.err.println(e1);
+                }
+                audioRecorder.stopRecording();
+                latch.countDown();
+            }
+            });
+        t.start();
+
+        //Wait for async tasks (recording) to finish
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
