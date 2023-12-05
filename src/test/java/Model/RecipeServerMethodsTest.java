ca2921e56300827135017e8ebe3c4d0008d3eab1
mBookUCSD <93008597+mBookUCSD@users.noreply.github.com>
2023-12-04 18:28:07 -0800

    SBST1/SBST2 MS2 Added End to End Tests
    and improved the Generate Demo

---
+++ src/test/java/Model/RecipeServerMethodsTest.java
@@ -0,0 +1,25 @@
+package Model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterEach;
+
+public class RecipeServerMethodsTest {
+    private MockRecipeServer mockServer;
+
+    @BeforeEach
+    void setUp() {
+        mockServer = new MockRecipeServer();
+    }
+
+    @Test
+    void testStartLoadStopServer() {
+        assertDoesNotThrow(() -> mockServer.startServer());
+        assertDoesNotThrow(() -> mockServer.loadServer());
+        assertDoesNotThrow(() -> mockServer.stopServer());
+    }
+
+}
